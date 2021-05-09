be9b992f06ba2f60cc97019746e3b5910db96583
sb9019 <sb9019@firstinfo.kr>
2021-05-09 23:10:42 +0900

    초기 업로드

---
+++ Slacker.py
@@ -0,0 +1,12 @@
+import requests
+ 
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+ 
+myToken = ""
+ 
+post_message(myToken,"#stock","jocoding")
